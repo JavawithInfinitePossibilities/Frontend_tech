d5359f58c54064c91a47970180c107c1af4fdc40
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2025-12-05 00:26:10 +0530

    The Complete JavaScript Course 2025: From Zero to Expert Section 1 and 2

---
+++ The_Complete_Javascript_Course/Section-02_JavaScript_Fundamentals_Part_1/script.js
@@ -0,0 +1,26 @@
+/**
+ *Linking a JavaScript File
+ */
+let js = "Amazing";
+if (js === "Amazing") {
+  alert("Javascript is fun");
+}
+console.log(40 + 43 - 45 - 45 + 1);
+/**
+ * Values and Variables
+ *  1.  Use camel casing
+ *  2.  variable should use alphabate, number and Underscore(_), dolor($)
+ *  3.  variable should not start with upperlatter.
+ *  4.  variable should not start with number.
+ *
+ */
+console.log("Jonas");
+
+let firstName = "bob";
+console.log(firstName);
+
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
